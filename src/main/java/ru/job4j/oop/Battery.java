8cc0b3449ff4eda2e7d29eab87f9442e6594d147
progspectar <progspectar@gmail.com>
2022-06-21 20:39:06 +0700

    Состояние объекта [#167388 #314939] - исправление3

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -2,18 +2,4 @@ package ru.job4j.oop;
 
 public class Battery {
 
-    private int load;
-
-    public Battery(int load) {
-        this.load = load;
-    }
-
-    public int getLoad() {
-        return load;
-    }
-
-    public void exchange(Battery another) {
-        another.load = another.load + this.load;
-        this.load = 0;
-    }
 }
