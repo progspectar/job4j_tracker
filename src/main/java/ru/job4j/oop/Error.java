83d3ef85081d424b2a8259ce7652626b24481487
progspectar <progspectar@gmail.com>
2022-06-21 11:03:27 +0700

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(true, 1, "Stack overflow");
+        error1.printInfo();
+        Error error2 = new Error(false, 0, "just warning: incorrect formatting of the paragraph");
+        error2.printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println("active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+    }
+}
