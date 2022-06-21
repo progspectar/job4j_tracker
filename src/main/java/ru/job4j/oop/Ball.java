e774045d7cbc9ac354bb7d81add5ae8032dd038b
progspectar <progspectar@gmail.com>
2022-06-21 10:48:51 +0700

    Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("the ball was eaten");
+        } else {
+            System.out.println("the ball ran away");
+        }
+    }
+}
+
+
+
