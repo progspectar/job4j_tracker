53379f6290a15da6e49271e085f25c962e64db15
progspectar <progspectar@gmail.com>
2022-06-23 14:29:49 +0700

    исправление предыдущего коммита №2

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-
 public class Item {
 
-    private final LocalDateTime created = LocalDateTime.now();
     private int id;
+
     private String name;
 
     public Item() {
@@ -18,10 +16,6 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
     }
 
     public int getId() {
