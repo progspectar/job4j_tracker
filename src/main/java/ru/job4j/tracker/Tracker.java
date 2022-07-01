eb5884dbc56639e89ee191db5d3ce3934a114313
progspectar <progspectar@gmail.com>
2022-07-01 11:55:58 +0700

    Метод удаления заявки Tracker.delete [#211749 #317587] - исправление

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,8 +59,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            int length = size - index - 1;
-            System.arraycopy(items, index + 1, items, index, length);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
