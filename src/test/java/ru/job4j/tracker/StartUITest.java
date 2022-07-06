217b449d35f85205fd505034d53bcaed97fe0c24
progspectar <progspectar@gmail.com>
2022-07-06 19:31:24 +0700

    Зависимость от System.out [#33568 #319383]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,4 +1,26 @@
 package ru.job4j.tracker;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StartUITest {
+
+    @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator()
+                      + "0. Exit" + System.lineSeparator()
+        ));
+    }
 }
