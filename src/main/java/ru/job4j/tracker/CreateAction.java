0455abee8e18c713dcf0024dc56efc90d28d60ce
progspectar <progspectar@gmail.com>
2022-07-06 19:11:37 +0700

    Зависимость от System.out [#33568 #319383]

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -1,23 +1,18 @@
 package ru.job4j.tracker;
 
 public class CreateAction implements UserAction {
-    private final Output out;
-
-    public CreateAction(Output out) {
-        this.out = out;
-    }
-
     @Override
     public String name() {
-        return "Create";
+        return "Add new Item";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        out.println("=== Create a new Item ====");
+        System.out.println("=== Create a new Item ===");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         tracker.add(item);
+        System.out.println("Добавленная заявка: " + item);
         return true;
     }
 }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,16 +1,10 @@
 package ru.job4j.tracker;
 
 public class StartUI {
-    private final Output out;
-
-    public StartUI(Output out) {
-        this.out = out;
-    }
-
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
-            this.showMenu(actions);
+            showMenu(actions);
             int select = input.askInt("Select: ");
             UserAction action = actions[select];
             run = action.execute(input, tracker);
@@ -18,20 +12,24 @@ public class StartUI {
     }
 
     private void showMenu(UserAction[] actions) {
-        out.println("Menu.");
+        System.out.println("Menu.");
         for (int index = 0; index < actions.length; index++) {
-            out.println(index + ". " + actions[index].name());
+            System.out.println(index + ". " + actions[index].name());
         }
     }
 
     public static void main(String[] args) {
-        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(output),
+                new CreateAction(),
+                new ShowAction(),
+                new ReplaceAction(),
+                new DeleteAction(),
+                new FindByIdAction(),
+                new FindByNameAction(),
                 new Exit()
         };
-        new StartUI(output).init(input, tracker, actions);
+        new StartUI().init(input, tracker, actions);
     }
 }
