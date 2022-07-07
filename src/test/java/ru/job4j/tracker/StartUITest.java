11e54e6bb2d36aff3abd7c364a87fd0489fae5ca
progspectar <progspectar@gmail.com>
2022-07-07 12:17:36 +0700

    Написать тесты на StartUI. [#785 #319367] - исправление

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,19 +6,20 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
-public class StartUITest {  @Test
-public void whenCreateItem() {
-    Input in = new StubInput(
-            new String[]{"0", "Item name", "1"}
-    );
-    Tracker tracker = new Tracker();
-    UserAction[] actions = {
-            new CreateAction(),
-            new Exit()
-    };
-    new StartUI().init(in, tracker, actions);
-    assertThat(tracker.findAll()[0].getName(), is("Item name"));
-}
+public class StartUITest {
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(
+                new String[]{"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new Exit()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
+    }
 
     @Test
     public void whenReplaceItem() {
@@ -26,7 +27,7 @@ public void whenCreateItem() {
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"0", replacedName, "1"}
+                new String[]{"0", String.valueOf(item.getId()), replacedName, "1"}
         );
         UserAction[] actions = {
                 new ReplaceAction(),
