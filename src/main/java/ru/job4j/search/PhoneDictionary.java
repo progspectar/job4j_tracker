91cc09ec61357d957b4e87cdd33b2cf1c93a1431
progspectar <progspectar@gmail.com>
2022-07-18 18:45:17 +0700

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,24 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private final ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : this.persons) {
+            if (person.getPhone().contains(key)
+                    || person.getAddress().contains(key)
+                    || person.getName().contains(key)
+                    || person.getSurname().contains(key)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
