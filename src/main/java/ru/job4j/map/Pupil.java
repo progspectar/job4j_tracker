724fecbcc7e9142d179eb2925b636f6b2c944b13
progspectar <progspectar@gmail.com>
2022-09-03 15:50:49 +0700

    Revert "3. Банковские переводы [#10038 #338869] исправление"
    This reverts commit a73c6cb537ebe6cb52d513e1a7ebda40f961f77a.

--- src/main/java/ru/job4j/map/Pupil.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j.map;
-
-import java.util.List;
-
-public record Pupil(String name, List<Subject> subjects) {
-}
--- src/main/java/ru/job4j/map/Subject.java
+++
@@ -1,4 +0,0 @@
-package ru.job4j.map;
-
-public record Subject(String name, int score) {
-}
