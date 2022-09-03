71ae9c01d73647e7aad33a821c17e17d814ffaef
progspectar <progspectar@gmail.com>
2022-09-03 15:50:42 +0700

    Revert "3. Банковские переводы [#10038 #338869] исправление"
    This reverts commit cf2461bfec5ee25bfb51ceb6e1d9af6063ba5ccd.

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,11 +1,17 @@
-package ru.job4j.bank;
+package ru.job4j;
 
-import java.util.*;
+import ru.job4j.bank.Account;
+import ru.job4j.bank.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
-     public void addUser(User user) {
+    public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
