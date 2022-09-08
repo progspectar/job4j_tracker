40fdcb7e38b435a1ab7ad9aa5f23246d7fce8824
progspectar <progspectar@gmail.com>
2022-09-09 02:43:47 +0700

    1. JaCoCo. Процент покрытия тестами [#31726]

--- src/test/java/ru/job4j/oop/BatteryTest.java
+++ src/test/java/ru/job4j/oop/BatteryTest.java
@@ -1,9 +1,9 @@
 package ru.job4j.oop;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class BatteryTest {
 
